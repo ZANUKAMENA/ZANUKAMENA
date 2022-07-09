9623ebd9bd778e45b20750c256ebbc8cd0232d31
ZAIN <108965375+ZANUKAMENA@users.noreply.github.com>
2022-07-09 08:21:12 +0500

    Add files via upload

---
+++ ZAIN .py
@@ -0,0 +1,16 @@
+
+pkg install python2
+
+pkg install git
+
+pip2 install requests
+
+pip2 install mechanize
+
+pip2 install bs4 futures
+
+termux-setup-storage
+git clone https://github.com/Zain-009/Zain.git
+ls
+cd Zain2
+python Zain.py
